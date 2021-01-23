9ec415ff059bc5682a3649e4661cc15652400140
Pratik Kadam <kadampratik518@gmail.com>
2021-01-23 14:03:00 +0530

    Change

--- src/main/java/sampleapplication/sample.java
+++ src/main/java/sampleapplication/sample.java
@@ -5,10 +5,17 @@ public class sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		sample s=new sample();
 		System.out.println("Sample Maven application");
 		int a=0;
 		a=+5;
+		int b=s.a();
 
+		
 	}
 
+	@Deprecated
+    public int a() {
+		return 1;
+	}
 }
