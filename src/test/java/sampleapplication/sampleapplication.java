4c29c8623ce40405718b0a30a6a3ceb2cc91cf1c
Pratik Kadam <kadampratik518@gmail.com>
2021-02-21 10:58:20 +0530

    Change

---
+++ src/test/java/sampleapplication/sampleapplication.java
@@ -0,0 +1,14 @@
+package sampleapplication;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sampleapplication {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented2");
+	}
+
+}
